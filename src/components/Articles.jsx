7e1b5282f92f0beb44314eaa83dbaaacc06dcd41
Gerard Byrne <gedbyrned@gmail.com>
2024-07-30 13:30:16 +0100

    add sorting queries to FE

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,45 +1,50 @@
-import { useState, useEffect } from "react";
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Articles = ({ articles }) => {
- 
+const Articles = ({ articles, setSortBy, setOrder }) => {
+  const [sortCriteria, setSortCriteria] = useState('created_at');
+  const [sortOrder, setSortOrder] = useState('desc');
+
+  const handleSortChange = (e) => {
+    const newSortCriteria = e.target.value;
+    setSortCriteria(newSortCriteria);
+    setSortBy(newSortCriteria);
+  };
+
+  const handleOrderChange = () => {
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortOrder(newSortOrder);
+    setOrder(newSortOrder);
+  };
+
   return (
     <main>
-    <button className="dropdown-button">Sort by dropdown...</button>
-  
-    <p>sort by date </p>
-    <p>sort by comment count</p>
-    <p>sort by votes</p>
-    <p>flip the order between ascending and descending </p>
-
-    <button>Ascending/Descending</button>
-    <br />
-    <br />
-
-
-
-    <article className="article-list">
-      <h2>All Articles</h2>
-    
-      <ul>
-        {articles.map((article) => {
-          return (
+      <div>
+        <select onChange={handleSortChange} value={sortCriteria}>
+          <option value="created_at">Sort by date</option>
+          <option value="comment_count">Sort by comment count</option>
+          <option value="votes">Sort by votes</option>
+        </select>
+        <button onClick={handleOrderChange}>
+          {sortOrder === 'asc' ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
+      <article className="article-list">
+        <h2>All Articles</h2>
+        <ul>
+          {articles.map((article) => (
             <li className="article-list" key={article.article_id}>
               <img src={article.article_img_url} alt={article.title} />
-              <br></br>
+              <br />
               <Link className="article-title" to={`/articles/${article.article_id}`}>
                 {article.title}
               </Link>
             </li>
-          );
-        })}
-      </ul>
-    </article>
-
+          ))}
+        </ul>
+      </article>
     </main>
   );
 };
 
 export default Articles;
-
